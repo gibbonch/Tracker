5394f82eac8f54b7125203da2593bae9cc5639ef
gibbonch <toropov280304@icloud.com>
2024-09-29 17:28:44 +0300

    Project configuration

---
+++ Tracker/Application/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Tracker
+//
+//  Created by Александр Торопов on 29.09.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+
+}
+
