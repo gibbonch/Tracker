e6c01d27a7e2e738043211d0a092af36bcf0ae2e
gibbonch <toropov280304@icloud.com>
2024-11-06 20:49:15 +0300

    Add assets

---
+++ Tracker/Resources/Typography.swift
@@ -0,0 +1,18 @@
+//
+//  Typography.swift
+//  Tracker
+//
+//  Created by Александр Торопов on 06.11.2024.
+//
+
+import UIKit
+
+extension UIFont {
+    static let ypBold34 = systemFont(ofSize: 34, weight: .bold)
+    static let ypBold32 = systemFont(ofSize: 32, weight: .bold)
+    static let ypBold19 = systemFont(ofSize: 19, weight: .bold)
+    static let ypRegular17 = systemFont(ofSize: 17, weight: .regular)
+    static let ypMedium16 = systemFont(ofSize: 16, weight: .medium)
+    static let ypMedium12 = systemFont(ofSize: 12, weight: .medium)
+    static let ypMedium10 = systemFont(ofSize: 10, weight: .medium)
+}
